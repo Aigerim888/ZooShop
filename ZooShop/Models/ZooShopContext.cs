1e7cb78e1fabfa8cc4f98e6151b35a6aace77e75
Aigerim <t_sa06@mail.ru>
2023-03-14 09:38:23 +0600

    Added model ZooShopContext

---
+++ ZooShop/Models/ZooShopContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZooShop.Models
+{
+    public class ZooShopContext:DbContext
+    {
+        public ZooShopContext(DbContextOptions<ZooShopContext>options):base(options) 
+        {
+
+        }
+        public DbSet<Brands> Brands { get; set; }   
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Departments> Departments { get; set; }
+        public DbSet<Employees> Employees { get; set; } 
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Storagees> Storagees { get; set; }
+    }
+    
+        
+    
+}
